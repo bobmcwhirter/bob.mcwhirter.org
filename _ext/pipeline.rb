395f6524b2c954198cf070c71248cb2064b003b3
Bob McWhirter <bob@mcwhirter.org>
2010-08-21 15:37:49 -0400

    First cut at site.

---
+++ _ext/pipeline.rb
@@ -0,0 +1,7 @@
+
+Awestruct::Extensions::Pipeline.new do
+  extension Awestruct::Extensions::Posts.new( '/blog' ) 
+  extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page=>5 )
+  extension Awestruct::Extensions::Indexifier.new
+end
+
---
+++ blog/2010-08-21-And-a-one-two-three-four.md
@@ -0,0 +1,7 @@
+---
+title: And a one, two, three, four!
+layout: blog
+---
+
+Just rebooting my blog.
+
