43b0d2e4e21d3782a7bedff5f5790aff4453d8a9
Bob McWhirter <bob@mcwhirter.org>
2010-08-23 10:31:06 -0400

    Work in auto-discovery of blog feed.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,11 +3,12 @@ require 'tagger'
 require 'awestruct/extensions/intense_debate'
 
 Awestruct::Extensions::Pipeline.new do
-  helper Awestruct::Extensions::IntenseDebate
 
   extension Awestruct::Extensions::Posts.new( '/blog' ) 
   extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page=>10 )
   extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page=>10 )
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/blog.atom', :num_entries=>20 )
+  extension Awestruct::Extensions::IntenseDebate.new
   extension Awestruct::Extensions::Indexifier.new
 end
 
