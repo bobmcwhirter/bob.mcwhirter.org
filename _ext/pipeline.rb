6a2ac00acb99ad2241853f165ae28fe3de474794
Bob McWhirter <bob@mcwhirter.org>
2010-08-23 14:08:14 -0400

    Move tagger over to awestruct.  Use generated tag-cloud extension now also.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,12 +1,9 @@
-require 'tagger'
-
-require 'awestruct/extensions/intense_debate'
 
 Awestruct::Extensions::Pipeline.new do
-
   extension Awestruct::Extensions::Posts.new( '/blog' ) 
   extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page=>10 )
   extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page=>10 )
+  extension Awestruct::Extensions::TagCloud.new( :posts, '/blog/tags/index.html', :layout=>:base, :title=>'Blog Tags' )
   extension Awestruct::Extensions::Atomizer.new( :posts, '/blog.atom', :num_entries=>20 )
   extension Awestruct::Extensions::IntenseDebate.new
   extension Awestruct::Extensions::Indexifier.new
